98f88ec56ea74abff3ab1556e375822c0b19aaff
Walter Oggioni <walter.oggioni@r3.com>
2020-12-17 11:04:21 +0100

    discovering quasar nuisances

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,15 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+        mavenCentral()
+        jcenter()
+    }
+    plugins {
+        id("org.jetbrains.kotlin.jvm") version "1.4.21" apply false
+    }
+}
+
 rootProject.name = "hello-quasar"
+
+include("suspendables")
